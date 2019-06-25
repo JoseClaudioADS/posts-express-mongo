7fc17873cd674b3f18592f8f6db220ba56a8f4bc
Jose Claudio <jcsj.ti@gmail.com>
2019-06-25 19:22:35 -0300

    Configurando mongoose

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+mongoose.connect('mongodb+srv://mongopost:postpost@cluster0-zjuyi.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useFindAndModify: true
+});
+
+
+async function testar() {
+    
+    const Test = mongoose.model('Test', {name: String});
+    
+    const t = new Test({name: "Testando mongoose"});
+    await t.save().then(() => {
+        console.log('Objeto criado');
+    })
+}
+
+testar();
 
 app.listen('3000');
